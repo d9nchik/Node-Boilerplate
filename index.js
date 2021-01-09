baf72b543a09709c2fdb924a06d50d4c03586f79
d9nich <dan.galaiko@gmail.com>
2021-01-09 15:00:58 +0000

    now prettier  and eslint works

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const PORT = /*process.env.PORT*/ 3000;
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
+    res.writeHead(200);
+    res.end('Hello, World!');
 };
 
 const server = http.createServer(requestListener);
